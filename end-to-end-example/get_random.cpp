bc5e0241c45e88eb75d3340bb0c03cff611b7e74
Samuel Delatore <samd@qrypt.com>
2022-08-31 16:40:43 +0000

    E2E ex cleanup, initializes token by itself

--- end-to-end-example/get_random.cpp
+++ end-to-end-example/get_random.cpp
@@ -1,53 +1,144 @@
 #include <iostream>
 #include <dlfcn.h>
+#include <memory>
+#include <cstring>
 
-#include "pkcs11.h"
+#include "cryptoki.h"
 
-const CK_SLOT_ID SLOT = 1653064145;
-const char *LIBRARY_PATH = "/workspaces/qryptoki/build/src/libqryptoki.so";
+#ifndef LIBRARY_PATH
+    #error LIBRARY_PATH not set
+#endif
 
 void *dl_handle = NULL;
+CK_FUNCTION_LIST_PTR functionList = NULL;
 
 using namespace std;
-
-void setDLHandle() {
-    dl_handle = dlopen(LIBRARY_PATH, RTLD_LAZY);
-
-    if(dl_handle == NULL)
-        throw runtime_error("dlopen returned NULL.");
-}
-
-void getFunction(void **ptr_to_fn, string fn_name) {
-    *ptr_to_fn = dlsym(dl_handle, fn_name.c_str());
-
-    if(*ptr_to_fn == NULL)
-        throw runtime_error(fn_name + " not found.");
-}
 
 string cryptokiFailErrorMsg(string fn_name, CK_RV rv) {
     return fn_name + " failed. rv = " + to_string(rv);
 }
 
-void initialize() {
-    CK_C_Initialize C_Initialize;
-    getFunction((void **)&C_Initialize, "C_Initialize");
+void setHandleAndFunctionList() {
+    dl_handle = dlopen(LIBRARY_PATH, RTLD_LAZY);
 
-    CK_RV rv = (*C_Initialize)(NULL_PTR);
+    if(dl_handle == NULL) {
+        const char *error_msg = dlerror();
+        
+        if(error_msg == NULL) {
+            throw runtime_error("dlopen returned NULL.");
+        } else {
+            throw runtime_error("dlopen failed: " + std::string(error_msg));
+        }
+    }
+    
+    CK_C_GetFunctionList C_GetFunctionList;
+    *(void **)&C_GetFunctionList = dlsym(dl_handle, "C_GetFunctionList");
+
+    if(C_GetFunctionList == NULL)
+        throw runtime_error("dlsym returned NULL.");
+    
+    CK_RV rv = (*C_GetFunctionList)(&functionList);
+    
+    if(rv != CKR_OK)
+        throw runtime_error(cryptokiFailErrorMsg("C_GetFunctionList", rv));
+    
+    if(functionList == NULL)
+        throw runtime_error("functionList is NULL even though C_GetFunctionList OK");
+}
+
+void initialize() {
+    CK_RV rv = functionList->C_Initialize(NULL_PTR);
 
     if(rv != CKR_OK)
         throw runtime_error(cryptokiFailErrorMsg("C_Initialize", rv));
 }
 
+bool isEmptySlot(CK_SLOT_ID slotID) {
+    CK_TOKEN_INFO info;
+    CK_RV rv = functionList->C_GetTokenInfo(slotID, &info);
+
+    return (rv == CKR_OK) && ((info.flags & CKF_TOKEN_INITIALIZED) == 0);
+}
+
+bool isEnd2EndSlot(CK_SLOT_ID slotID) {
+    CK_TOKEN_INFO info;
+    CK_RV rv = functionList->C_GetTokenInfo(slotID, &info);
+
+    char *label = (char *)info.label;
+    return strncmp(label, "End-2-End Example Token         ", 32) == 0;
+}
+
+CK_SLOT_ID newEnd2EndSlot() {
+    CK_SLOT_ID slotID;
+    CK_RV rv;
+
+    // slotCount = total # of slots (empty + containing tokens)
+    CK_ULONG slotCount;
+    rv = functionList->C_GetSlotList(CK_FALSE, NULL_PTR, &slotCount);
+    if(rv != CKR_OK)
+        throw runtime_error(cryptokiFailErrorMsg("C_GetSlotList", rv));
+    
+    // slotList = list of all slots
+    unique_ptr<CK_SLOT_ID[]> slotList = make_unique<CK_SLOT_ID[]>(slotCount);
+
+    rv = functionList->C_GetSlotList(CK_TRUE, (CK_SLOT_ID_PTR)slotList.get(), &slotCount);
+    if(rv != CKR_OK)
+        throw runtime_error(cryptokiFailErrorMsg("C_GetSlotList", rv));
+    
+    for(CK_ULONG i = 0; i < slotCount; i++) {
+        slotID = slotList[i];
+
+        if(!isEmptySlot(slotID)) continue;
+
+        CK_UTF8CHAR_PTR soPin = (CK_UTF8CHAR_PTR)"end2endso";
+        CK_ULONG soPinLen = strlen((char *)soPin);
+
+        CK_UTF8CHAR_PTR label = (CK_UTF8CHAR_PTR)"End-2-End Example Token         ";
+        if(strlen((char *)label) != 32)
+            throw runtime_error("label must have length 32");
+        
+        rv = functionList->C_InitToken(slotID, soPin, soPinLen, label);
+        if(rv == CKR_OK) return slotID;
+
+        throw runtime_error(cryptokiFailErrorMsg("C_InitToken", rv));
+    }
+
+    throw runtime_error("No existing E2E token and no empty slots.");
+}
+
+CK_SLOT_ID getEnd2EndSlot() {
+    CK_SLOT_ID slotID;
+    CK_RV rv;
+
+    // slotCount = # of slots containing tokens
+    CK_ULONG slotCount;
+    rv = functionList->C_GetSlotList(CK_TRUE, NULL_PTR, &slotCount);
+    if(rv != CKR_OK)
+        throw runtime_error(cryptokiFailErrorMsg("C_GetSlotList", rv));
+
+    // slotList = list of slots containing tokens
+    unique_ptr<CK_SLOT_ID[]> slotList = make_unique<CK_SLOT_ID[]>(slotCount);
+
+    rv = functionList->C_GetSlotList(CK_TRUE, (CK_SLOT_ID_PTR)slotList.get(), &slotCount);
+    if(rv != CKR_OK)
+        throw runtime_error(cryptokiFailErrorMsg("C_GetSlotList", rv));
+
+    for(CK_ULONG i = 0; i < slotCount; i++) {
+        slotID = slotList[i];
+
+        if(isEnd2EndSlot(slotID)) return slotID;
+    }
+
+    return newEnd2EndSlot();
+}
+
 CK_SESSION_HANDLE openSession(CK_SLOT_ID slotID) {
     CK_SESSION_HANDLE session;
-
-    CK_C_OpenSession C_OpenSession;
-    getFunction((void **)&C_OpenSession, "C_OpenSession");
 
     CK_FLAGS sessionFlags = CKF_SERIAL_SESSION; // In particular, read-only.
     CK_NOTIFY myNotify = NULL_PTR; // For now. Should read up on callback.
 
-    CK_RV rv = (*C_OpenSession)(slotID, sessionFlags, NULL_PTR, NULL_PTR, &session);
+    CK_RV rv = functionList->C_OpenSession(slotID, sessionFlags, NULL_PTR, NULL_PTR, &session);
 
     if(rv != CKR_OK)
         throw runtime_error(cryptokiFailErrorMsg("C_OpenSession", rv));
@@ -55,39 +146,17 @@ CK_SESSION_HANDLE openSession(CK_SLOT_ID slotID) {
     return session;
 }
 
-void login(CK_SESSION_HANDLE session) {
-    CK_C_Login C_Login;
-    getFunction((void **)&C_Login, "C_Login");
-
-    const size_t userPinLen = 4;
-    CK_UTF8CHAR userPin[userPinLen] = {'u', 's', 'e', 'r'};
-
-    CK_RV rv = (*C_Login)(session, CKU_USER, userPin, userPinLen);
-
-    if(rv != CKR_OK)
-        throw runtime_error(cryptokiFailErrorMsg("C_Login", rv));
-}
-
 void generateRandom(CK_SESSION_HANDLE session, CK_BYTE_PTR data, CK_ULONG len) {
-    CK_C_GenerateRandom C_GenerateRandom;
-    getFunction((void **)&C_GenerateRandom, "C_GenerateRandom");
-
-    CK_RV rv = (*C_GenerateRandom)(session, data, len);
+    CK_RV rv = functionList->C_GenerateRandom(session, data, len);
 
     if(rv != CKR_OK)
         throw runtime_error(cryptokiFailErrorMsg("C_GenerateRandom", rv));
 }
 
 void finalize() {
-    CK_C_Finalize C_Finalize;
+    if(functionList == NULL) return;
 
-    try {
-        getFunction((void **)&C_Finalize, "C_Finalize");
-    } catch (...) {
-        return;
-    }
-
-    CK_RV rv = (*C_Finalize)(NULL_PTR);
+    CK_RV rv = functionList->C_Finalize(NULL_PTR);
 
     if(rv != CKR_OK)
         cout << cryptokiFailErrorMsg("C_Finalize", rv) << endl;
@@ -99,13 +168,15 @@ void cleanup() {
 }
 
 int main() {
+    CK_SLOT_ID slotID;
     CK_SESSION_HANDLE session;
 
     try {
-        setDLHandle();
+        setHandleAndFunctionList();
         initialize();
-        session = openSession(SLOT);
-        login(session);
+
+        slotID = getEnd2EndSlot();
+        session = openSession(slotID);
 
         const CK_ULONG len = 1024;
         CK_BYTE data[len];
